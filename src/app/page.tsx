d8b11f5679473a23ecd06784fb799b889325af00
nataliafrancisca <nathifrancisca@gmail.com>
2024-07-22 21:34:25 -0300

    feat: create page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main>
-      <h1>HI?</h1>
+    <main className="page__home">
+      <Image
+        className="image__illustration"
+        src="images/illustration.svg"
+        alt="ilustração de uma mulher usando fones de ouvido e mexendo no notebook que está sobre suas pernas"
+        width={300}
+        height={364}
+      />
+
+      <section className="section__elements">
+        <h1>
+          CONEXÃO <br /> BRASIL
+        </h1>
+        <p>Seu dicionário pessoal, simples e eficaz.</p>
+
+        <button
+          role="button"
+          onClick={() => router.push('/login')}
+          className="button-nav"
+        >
+          INICIAR
+        </button>
+      </section>
     </main>
   );
 }

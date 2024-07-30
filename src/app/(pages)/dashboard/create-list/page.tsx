2abfbba8c34b78aea45131abbf74513b2d09fa34
nataliafrancisca <nathifrancisca@gmail.com>
2024-07-29 23:21:24 -0300

    feat: create page dashboard and sub-pages

---
+++ src/app/(pages)/dashboard/create-list/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { useCreateListLogic } from './useCreateListLogic';
+import Alert from '@/components/Alert/Alert';
+import Loader from '@/components/Loader/Loader';
+
+const Page = () => {
+  const { loading, alertMessage, setListTitle, onSubmit } =
+    useCreateListLogic();
+
+  return (
+    <main className="page__create-list">
+      <header className="page__header">
+        <h1>Criar Lista:</h1>
+        <p>
+          Escolha o <b className="__styled">nome</b> da sua nova lista:
+        </p>
+      </header>
+
+      {alertMessage && (
+        <Alert message={alertMessage.message} status={alertMessage.status} />
+      )}
+
+      {loading && <Loader />}
+
+      <form onSubmit={async (e) => await onSubmit(e)}>
+        <input
+          type="text"
+          placeholder="blink-182 adam's song"
+          required
+          minLength={4}
+          maxLength={50}
+          onChange={(e) => setListTitle(e.target.value)}
+        />
+
+        <button type="submit">Criar Lista</button>
+      </form>
+    </main>
+  );
+};
+
+export default Page;

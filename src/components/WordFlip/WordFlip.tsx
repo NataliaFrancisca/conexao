71baa951e10538e76c831fcaa456275c2fa9ccdd
nataliafrancisca <nathifrancisca@gmail.com>
2024-08-07 23:45:50 -0300

    feat: use input type checkbox

--- src/components/WordFlip/WordFlip.tsx
+++ src/components/WordFlip/WordFlip.tsx
@@ -5,15 +5,17 @@ const WordFlip = (props: { data: IWord }) => {
 
   return (
     <article className="component__word-flip">
-      <section className="flip-inner">
+      <input className="flip-inner" type="checkbox" id="flip-inner"></input>
+
+      <label className="flip-inner" htmlFor="flip-inner">
         <div className="flip-front">
           <h1>{original}</h1>
         </div>
 
-        <div className="flip-back ">
+        <div className="flip-back">
           <h1>{translated}</h1>
         </div>
-      </section>
+      </label>
     </article>
   );
 };

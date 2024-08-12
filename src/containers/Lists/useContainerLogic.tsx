fcc2e2566cbc3cb949c2c2e98d5777511efd0a5f
nataliafrancisca <nathifrancisca@gmail.com>
2024-08-12 19:42:58 -0300

    feat: set an alert message if response is undefined

--- src/containers/Lists/useContainerLogic.tsx
+++ src/containers/Lists/useContainerLogic.tsx
@@ -1,13 +1,21 @@
 import { ListsController } from '@/controllers/ListsController';
-import { IList } from '@/utils/ts/interface';
+import { IAlertMessage, IList } from '@/utils/ts/interface';
 import { useEffect, useState } from 'react';
 
 export const useContainerLogic = () => {
   const [lists, setLists] = useState<IList[]>([]);
+  const [alertMessage, setAlertMessage] = useState<null | IAlertMessage>(null);
 
   const getLists = async () => {
     const controller = new ListsController();
     const response = await controller.fetchLists();
+
+    if (response.requestWasSuccess === undefined) {
+      setAlertMessage({
+        message: response.message,
+        status: response.requestWasSuccess,
+      });
+    }
 
     if (response.data) {
       setLists(response.data);
@@ -18,5 +26,5 @@ export const useContainerLogic = () => {
     getLists();
   }, []);
 
-  return { lists };
+  return { lists, alertMessage };
 };
